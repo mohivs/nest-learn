45e5cc32a3dea74a270ef958158749ee1bac14a5
MohiVs <mohisls2012@gmail.com>
2024-10-21 14:04:43 +0330

    sdf

--- src/users/Dtos/create-user.dto.ts
+++ src/users/Dtos/create-user.dto.ts
@@ -1,14 +1,19 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @MinLength(3, { message: 'حداقل سه کلمه بنویسید' })
   firstName: string;
+
   @IsString()
   lastName: string;
-  @IsString()
+
   @IsNotEmpty()
+  @IsEmail()
+  @MinLength(8)
   email: string;
+
   @IsString()
   @IsNotEmpty()
   password: string;
